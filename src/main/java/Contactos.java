00c869226285de0aff1e27b29704f5c9fbb92dae
jellalshadows <dasilvaadrian497@gmail.com>
2024-04-08 08:50:34 +0200

    ListaContactosV1.0

---
+++ src/main/java/Contactos.java
@@ -0,0 +1,44 @@
+public class Contactos {
+    private String name;
+    private String email;
+    private String phone_number;
+
+    public Contactos(String name, String email, String phone_number) {
+        this.name = name;
+        this.email = email;
+        this.phone_number = phone_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    @Override
+    public String toString() {
+        return "Contactos{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone_number='" + phone_number + '\'' +
+                '}';
+    }
+}
